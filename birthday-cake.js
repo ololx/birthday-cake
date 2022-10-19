1eed31e24df4459b3b14b0e68c8b97ca2eb61d7b
ololx <ololx@icloud.com>
2022-10-19 18:29:02 +0500

    fix(birthday-cake.js) fix candle click

--- birthday-cake.js
+++ birthday-cake.js
@@ -37,7 +37,7 @@ function appendCandles(candlesCount) {
 		document.body.innerHTML += `<div id="candle_${i}" class="candle" style="margin-left:${candleXPosition}px; margin-top:${candleYPosition}px;"></div>`;
 		
 		let candle = document.getElementById(`candle_${i}`);
-		candle.setAttribute("onClick", "putOutCandle(candle_${i});");
+		candle.setAttribute("onClick", `putOutCandle("candle_${i}");`);
 
 		for (var j = 0; j < 5; j++) {
 			candle.innerHTML += `<div class="flame"></div>`;
@@ -45,8 +45,10 @@ function appendCandles(candlesCount) {
 	}
 }
 
-function putOutCandle(candle) {
-	if (candle == null) return;
+function putOutCandle(candle_name) {
+	if (candle_name == null) return;
+
+	let candle = document.getElementById(candle_name);
 
 	for (var i = 0; i < 5; i++) {
 		var flame = candle.querySelector(`.flame`);
